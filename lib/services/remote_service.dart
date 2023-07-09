ea89eaf08b9f697b79185b65641d3cc192059450
Oreoluwa <Odetundeoreoluwadavid@gmail.com>
2023-07-09 12:38:49 +0100

    Added Product Http Get Request to the server.

---
+++ lib/services/remote_service.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+import 'package:shop_it_grocery_app/model/product_model.dart';
+import 'package:http/http.dart' as http;
+
+class ProductRepository {
+Future<Product> fetchProduct() async {
+
+    final response = await http.get(
+      Uri.parse(" https://makeup-api.herokuapp.com/api/v1/products.json?brand")
+      );
+
+    if(response.statusCode == 200) {
+      return Product.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to fetch product data');
+    }
+    
+  }
+}

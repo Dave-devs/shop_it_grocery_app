d2351e375be18090ab87bcbafa7b38d2eb404bfb
Oreoluwa <Odetundeoreoluwadavid@gmail.com>
2023-07-09 12:49:50 +0100

    Refactored get request

--- lib/services/remote_service.dart
+++ lib/services/remote_service.dart
@@ -3,10 +3,9 @@ import 'package:shop_it_grocery_app/model/product_model.dart';
 import 'package:http/http.dart' as http;
 
 class ProductRepository {
-Future<Product> fetchProduct() async {
-
+Future<Product> fetchProduct(String brand) async {
     final response = await http.get(
-      Uri.parse(" https://makeup-api.herokuapp.com/api/v1/products.json?brand")
+      Uri.parse(" https://makeup-api.herokuapp.com/api/v1/products.json?(brand)")
       );
 
     if(response.statusCode == 200) {
